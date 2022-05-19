6ee41faac10abdc7877e2521ffef7e4ba61ff75b
gabrieldsinfo <gabrieldsinfo@gmail.com>
2022-05-18 21:29:40 -0300

    CLASSE CRUD COM ATRIBUTOS

---
+++ hotel.py
@@ -0,0 +1,64 @@
+#02.HOTEL
+#CLASSE CRUD COM ATRIBUTOS.
+#reqparse = RECEBE OS ELEMENTOS JSON DA REQUISICAO
+
+#EXEMPLO DE ARQUIVO JSON UTILIZADO NO METODO POST PARA TESTES VIA API:
+"""
+            {
+            "nome": "Bravo Hotel",
+            "estrelas": 2.2,
+            "diaria": 222.34,
+            "cidade": "Sao Paulo"
+            }
+"""
+
+from flask_restful import Resource, reqparse
+from models.hotelmodels import HotelModel
+
+class Hoteis(Resource):
+    def get(self):
+        return{'hoteis':[hotel.json() for hotel in HotelModel.query.all()]} #SELECT * FROM Hoteis
+
+class Hotel(Resource):
+    atributos = reqparse.RequestParser()
+    atributos.add_argument('nome')
+    atributos.add_argument('estrelas')
+    atributos.add_argument('diaria')
+    atributos.add_argument('cidade')
+
+
+    def get(self, hotel_id):
+        hotel = HotelModel.find_hotel(hotel_id)
+        if hotel:
+            return hotel.json()
+        return {'message': 'Hotel not found.'}, 404 #not found
+
+
+    def post(self, hotel_id):
+        if HotelModel.find_hotel(hotel_id):
+            return {"message": "Hotel id '{}' already exists.".format(hotel_id)}, 400 #Badrequest
+
+        dados = Hotel.atributos.parse_args()
+        hotel = HotelModel(hotel_id, **dados)
+        hotel.save_hotel()
+        return hotel.json()
+
+           
+    def put(self, hotel_id):
+        dados = Hotel.atributos.parse_args()
+        hotel_encontrado = HotelModel.find_hotel(hotel_id)
+        if hotel_encontrado:
+            hotel_encontrado.update_hotel(**dados)
+            hotel_encontrado.save_hotel()
+            return hotel_encontrado.json(), 200 # OK
+        hotel = HotelModel(hotel_id, **dados)
+        hotel.save_hotel()
+        return hotel.json(), 201
+
+
+    def delete(self, hotel_id):
+        hotel = HotelModel.find_hotel(hotel_id)
+        if hotel:
+            hotel.delete_hotel()
+            return {'message': 'Hotel deleted.'}
+        return {'message': 'Hotel not found.'}, 404
